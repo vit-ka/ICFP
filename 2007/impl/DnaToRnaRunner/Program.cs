ade14635fbe522970c61d51a93549b090056373b
Vitaly Kalinkin <lattyf@gmail.com>
2009-01-21 12:16:42 +0400

    Проверка аргументов в тестовой программе запуска Rna.
    Signed-off-by: Vitaly Kalinkin <lattyf@gmail.com>

--- 2007/impl/DnaToRnaRunner/Program.cs
+++ 2007/impl/DnaToRnaRunner/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace DnaToRnaRunner
+﻿using System;
+
+namespace DnaToRnaRunner
 {
-    internal class Program
+    public class Program
     {
+        private static string _dnaFile;
+        private static string _rnaFile;
+
         private static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: DnaToRnaRunner.exe <dna-file> <output-rna-file>");
+                return;
+            }
+
+            _dnaFile = args[0];
+            _rnaFile = args[1];
         }
     }
 }

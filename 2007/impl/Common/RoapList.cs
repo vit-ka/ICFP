76b78e3fdfb53706aaf71d50d9c3c48d9378b452
Vitaly Kalinkin <lattyf@gmail.com>
2009-01-21 16:57:14 +0400

    Написаны кое-какие функциональные тесты  для RoapList.

--- 2007/impl/Common/RoapList.cs
+++ 2007/impl/Common/RoapList.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Common
 {
     /// <summary>
@@ -10,8 +14,56 @@ namespace Common
     /// конечную информацию. Первичный список используется для быстрой обработки массивов (сдвига, удаление, создания).
     /// </summary>
     /// <typeparam name="ItemType">Тип элемента списка.</typeparam>
-    public class RoapList<ItemType>
+    public class RoapList<ItemType> : IEnumerable<ItemType>
     {
-        
+        public void Add(int i)
+        {
+        }
+
+        public int Length
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public void AddRange(ICollection<ItemType> list)
+        {
+            
+        }
+
+        public void RemoveAt(int i)
+        {
+            
+        }
+
+        public void RemoveRange(int i, int i1)
+        {
+            
+        }
+
+        public ItemType this[int index]
+        {
+            get
+            {
+                return default(ItemType);
+            }
+
+            set
+            {
+                
+            }
+        }
+
+        public IEnumerator<ItemType> GetEnumerator()
+        {
+            return new List<ItemType>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }

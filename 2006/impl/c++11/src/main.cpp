bc469b5ef0dee47ad5ae3b06cd8d63a45e906086
Vitaly Kalinkin <vitaly@kalinkin.me>
2015-10-19 23:39:07 +0100

    Added gflags library to parse command line flags.

--- 2006/impl/c++11/src/main.cpp
+++ 2006/impl/c++11/src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include <gflags/gflags.h>
+
+DEFINE_string(load, "", "Dump file name to load.");
+
+static const char* VERSION = "0.0.1";
+static const char* USAGE_MESSAGE = "[flags] scroll_file.um";
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  cout << "Hello UM world!" << endl;
+  google::SetVersionString(VERSION);
+  google::SetUsageMessage(USAGE_MESSAGE);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (FLAGS_load != "") {
+    cout << "Dump file loading mode. I will load '" << FLAGS_load 
+         << "' file and continue execution from the previously saved state." << endl;
+  } else {
+    cout << "Normal startup mode. Image file name to execute: '" << argv[1] << "'" << endl;
+  }
 }

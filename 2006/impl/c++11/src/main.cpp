38942c9a667a4a027fdc1da7597f0d22e823c890
Vitaly Kalinkin <vitaly@kalinkin.me>
2015-10-20 11:37:25 +0100

    Added glog dependency.

--- 2006/impl/c++11/src/main.cpp
+++ 2006/impl/c++11/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <gflags/gflags.h>
+#include <glog/logging.h>
 
 DEFINE_string(load, "", "Dump file name to load.");
 
@@ -13,11 +14,16 @@ int main(int argc, char* argv[]) {
   google::SetVersionString(VERSION);
   google::SetUsageMessage(USAGE_MESSAGE);
   google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
 
   if (FLAGS_load != "") {
-    cout << "Dump file loading mode. I will load '" << FLAGS_load 
-         << "' file and continue execution from the previously saved state." << endl;
+    LOG(INFO) << "Dump file loading mode. I will load '" << FLAGS_load
+              << "' file and continue execution from the previously saved state.";
   } else {
-    cout << "Normal startup mode. Image file name to execute: '" << argv[1] << "'" << endl;
+    if (argc < 2) {
+      LOG(FATAL) << "The scroll file name should be specified as the first argument.";
+    }
+
+    LOG(INFO) << "Normal startup mode. Image file name to execute: '" << argv[1] << "'";
   }
 }

4307d2e11c2d484df868c2a22bf0deab10d15af6
Vitalii Kalinkin <vitalii@kalinkin.email>
2024-02-14 17:07:19 -0800

    Endiness

--- 2006/impl/c++_new/main.cpp
+++ 2006/impl/c++_new/main.cpp
@@ -6,6 +6,8 @@
 
 #include "fmt/core.h"
 
+enum Op { Orthography = 13 };
+
 std::vector<uint32_t> readScroll(const std::string &file) {
   std::vector<uint32_t> result;
   std::ifstream program(file);
@@ -16,6 +18,12 @@ std::vector<uint32_t> readScroll(const std::string &file) {
 
     result.resize(len);
     program.read(reinterpret_cast<char *>(result.data()), len);
+
+    // Swapping endiness
+    for (auto &v : result) {
+      unsigned char *pV = reinterpret_cast<unsigned char *>(&v);
+      std::reverse(pV, pV + sizeof(v));
+    }
 
     if (program) {
       std::cerr << "\tRead " << len << " bytes of scroll " << file << std::endl;
@@ -44,14 +52,30 @@ int main(int argc, char *argv[]) {
 
   while (true) {
     auto platter = mem[0][ip];
-    auto op = (platter & 0xf0000000) >> 27;
+    auto op = static_cast<Op>((platter & 0xf0000000) >> 28);
     size_t a, b, c;
     a = platter & 0x7;
     b = (platter & 0x38) >> 3;
     c = (platter & 0x1c0) >> 6;
 
-    std::cerr << "[" << fmt::format("{:0x}", ip) << "] Read op #" << op << "("
-              << a << ";" << b << ";" << c << ")" << std::endl;
+    switch (op) {
+    case Orthography: {
+      a = (platter & 0xe000000) >> 25;
+      size_t value = platter & 0x1ffffff;
+      std::cerr << "[" << fmt::format("0x{:08x}", ip) << "]["
+                << fmt::format("0x{:08x}", platter) << "] opcode #" << op << "("
+                << a << "). Value: " << fmt::format("0x{:08x}", value)
+                << std::endl;
+      rs[a] = value;
+      ++ip;
+      break;
+    }
+    default:
+      std::cerr << "Unknown opcode: [" << fmt::format("0x{:08x}", ip) << "]["
+                << fmt::format("0x{:08x}", platter) << "] opcode #" << op << "("
+                << a << ";" << b << ";" << c << ")" << std::endl;
+      return -1;
+    }
   }
 
   return 0;
